bc8fddcd876b532512c38f6d6082f4c27ec9e6e6
liutong12345 <2023252885@qq.com>
2020-05-11 13:16:03 +0800

    第一次上传

---
+++ vuedemo3/src/main/java/cn/kj/vuedemo/domain/User.java
@@ -0,0 +1,82 @@
+package cn.kj.vuedemo.domain;
+
+public class User {
+    private Integer id;
+    private Integer age;
+    private String username;
+    private String password;
+    private String email;
+    private String sex;
+
+    public User() {
+    }
+
+    public User(Integer id, Integer age, String username, String password, String email, String sex) {
+        this.id = id;
+        this.age = age;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.sex = sex;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", age=" + age +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", sex='" + sex + '\'' +
+                '}';
+    }
+}
